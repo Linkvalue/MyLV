6e075d4b884dce7a36f52de124b9f70c5eb7424e
Jacques Latrive <jacques.latrive@gmail.com>
2016-06-30 15:29:43 +0200

    Add calendar component

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,3 +1,9 @@
 import React from 'react'
+import Calendar from './calendar'
 
-export const App = (props) => <h1>Hello, world!</h1>
+export const App = (props) => (
+  <div>
+    <h1>Hello, world!</h1>
+    <Calendar month='2016-06' day={29}/>
+  </div>
+)
