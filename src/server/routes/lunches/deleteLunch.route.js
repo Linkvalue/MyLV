918610e8d06b2d2cd23610a81c226f499ff8b777
d34thwings <d34thwings@gmail.com>
2017-10-19 09:10:17 +0000

    fix(lint): Fix JS linting

--- src/server/routes/lunches/deleteLunch.route.js
+++ src/server/routes/lunches/deleteLunch.route.js
@@ -3,7 +3,7 @@ const Boom = require('boom')
 module.exports = {
   method: 'DELETE',
   path: '/api/lunches/{id}',
-  handler(req, res) {
+  handler (req, res) {
     req.server.app.models.Lunch.deleteOne({ _id: req.params.id })
       .then(() => res({ deleted: true }))
       .catch(err => Boom.wrap(err))
