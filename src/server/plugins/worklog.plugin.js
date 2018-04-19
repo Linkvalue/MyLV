2397990b46b2a0ec989915d104d9f75b714cf042
d34thwings <d34thwings@gmail.com>
2018-04-19 13:49:40 +0000

    feat(cron): Add incomplete worklog notify cron

--- src/server/plugins/worklog.plugin.js
+++ src/server/plugins/worklog.plugin.js
@@ -1,6 +1,10 @@
+const moment = require('moment')
+
+const getWorkingDays = require('../helpers/getWorkingDays.helper')
+const lvConnect = require('../helpers/lvconnect.helper')
+
 exports.register = (server, options, next) => {
   server.expose('saveEntries', async (entries, userId) => {
-    console.log(userId)
     const entriesToAdd = entries
       .map(entry => Object.assign(entry, { userId }))
       .filter(entry => !!entry.label)
@@ -13,6 +17,29 @@ exports.register = (server, options, next) => {
     if (entriesToAdd.length > 0) {
       await server.app.models.Entry.create(entriesToAdd)
     }
+  })
+
+  server.expose('notifityIncompleteWorklog', async () => {
+    const date = moment().format('YYYY-MM')
+    const entries = await server.app.models.Entry.find({
+      date: { $regex: date },
+    })
+
+    const expectedDays = getWorkingDays(date)
+    const partners = new Map()
+    entries.forEach(entry => partners.set(entry.userId.toString(), (partners.get(entry.userId.toString()) || 0) + 1))
+    const filled = Array.from(partners.entries())
+      .filter(([, count]) => count / 2 >= expectedDays)
+      .map(([userId]) => userId)
+
+    const badBoys = await server.app.models.Profile.find({ userId: { $nin: filled } })
+    const badBoysQuery = badBoys.map(profile => `ids=${profile.userId.toString()}`)
+    const { results } = await lvConnect.api(`/users?${badBoysQuery.join('&')}`).then(res => res.json())
+    const reelBadBoys = results.filter(partner => partner.roles.includes('tech'))
+
+    await server.methods.sendPushNotification(reelBadBoys.map(partner => partner.id), JSON.stringify({
+      message: 'CRA CRA CRA CRA !',
+    }))
   })
 
   next()
